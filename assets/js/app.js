1d01d7a5606ba15cf918a64ef7a526f8bf28f5cf
David Martin-Corral <david@zenseiapp.com>
2021-05-31 17:33:48 +0200

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,8 @@ var language = navigator.language || navigator.browserLanguage;
 
 // alert(language);
 
-if (language.indexOf('es') > -1) {
-  document.location.href = 'http://kattumaramtarifa.com/';
-} else {
-  document.location.href = 'http://kattumaramtarifa.com/en';
-}
+// if (language.indexOf('es') > -1) {
+//   document.location.href = 'http://kattumaramtarifa.com/';
+// } else {
+//   document.location.href = 'http://kattumaramtarifa.com/en';
+// }
